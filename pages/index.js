cab9f9327e9707e2bc1f40cce43fbffe44f118b1
Marcell Janovszki <marcell.janovszki@gmail.com>
2021-08-08 18:27:49 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+// STYLED-COMPONENTS
+import styled from "styled-components"
+
+const Wrapper = styled.div``
+
+const HomePage = () => {
+    return (
+        <Wrapper>
+            home_page
+        </Wrapper>
+    )
+}
+
+export default HomePage
